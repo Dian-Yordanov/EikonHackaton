96d0775b7d0f6cc532ab17ea3568871954652af5
Xynnect <dido.jordanov7@gmail.com>
2014-03-22 21:44:57 +0000

    dsadasdas

--- PersonalFeed/src/com/example/personalfeed/NewsPage.java
+++ PersonalFeed/src/com/example/personalfeed/NewsPage.java
@@ -1,8 +1,12 @@
 package com.example.personalfeed;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class NewsPage extends Activity {
@@ -15,21 +19,23 @@ public class NewsPage extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.news_page);
-		
+
 		gridLayout = (GridLayout) findViewById(R.id.GridLayout1);
-		
-		
-		
+
+		ImageView pfheader = (ImageView) findViewById(R.id.pfheader);
+		Bitmap header = BitmapFactory.decodeResource(getBaseContext()
+				.getResources(), R.drawable.pfheader);
+		Bitmap resizedBitmapFlag = Bitmap.createScaledBitmap(header,
+				MainActivity.width * 49 / 100, 80, true);
+		pfheader.setImageBitmap(resizedBitmapFlag);
+
 		InflatedViewForNewsPage inflatedNewsView = new InflatedViewForNewsPage(
 				getApplicationContext());
-		
 
 		gridLayout.addView(inflatedNewsView.inflator(getApplicationContext()));
 		gridLayout.addView(inflatedNewsView.inflator(getApplicationContext()));
 		gridLayout.addView(inflatedNewsView.inflator(getApplicationContext()));
 		gridLayout.addView(inflatedNewsView.inflator(getApplicationContext()));
-		
-		
 
 	}
 
