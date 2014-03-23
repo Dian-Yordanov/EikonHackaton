a3a1e646818e8faa25c470e5df0b92009d317b5a
Xynnect <dido.jordanov7@gmail.com>
2014-03-23 08:29:12 +0000

    asdasd

--- PersonalFeed/src/com/example/personalfeed/ArticleView.java
+++ PersonalFeed/src/com/example/personalfeed/ArticleView.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,9 +30,15 @@ public class ArticleView  extends Activity {
 				MainActivity.width * 42 / 100, 60, true);
 		pfheader.setImageBitmap(resizedBitmapFlag);
 		
+		   WebView thomsonReuters = (WebView) findViewById(R.id.thomsonReutersSiteView );
+		   thomsonReuters.getSettings().setJavaScriptEnabled(true);
+		  
+		  
+		   thomsonReuters.setWebViewClient(new WebViewClient());
+		   thomsonReuters.loadUrl("http://www.reuters.com/article/2014/03/22/us-baseball-openers-idUSBREA2K0SJ20140322");
 
-		Button shareOnTwitter = (Button) findViewById(R.id.shareOnTwitter);
-		
+
+		Button shareOnTwitter = (Button) findViewById(R.id.shareOnTwitter);		
 		shareOnTwitter.setOnClickListener(new OnClickListener() {
 
 			@Override
