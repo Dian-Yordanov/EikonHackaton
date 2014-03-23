e6b2b25421e85e093cfe6d277fb4b933f3e8dff1
Xynnect <dido.jordanov7@gmail.com>
2014-03-23 03:35:54 +0000

    novoda and multi threating done

--- PersonalFeed/src/com/example/personalfeed/InflatedViewForNewsPage.java
+++ PersonalFeed/src/com/example/personalfeed/InflatedViewForNewsPage.java
@@ -2,9 +2,12 @@ package com.example.personalfeed;
 
 import java.util.ArrayList;
 
+import com.novoda.imageloader.core.util.DirectLoader;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -12,25 +15,28 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class InflatedViewForNewsPage extends LinearLayout  {
+public class InflatedViewForNewsPage extends LinearLayout {
 	public static View inflatedViewNews;
 	public static LayoutInflater newsView;
 	public static LinearLayout mainLayout;
 	static ImageView newsPicture;
-	
+
 	public static String urlForPictureForThread;
 
 	public InflatedViewForNewsPage(Context context) {
 		super(context);
 	}
 
-	public View inflator(Context context, String title) {
-		
+	public View inflator(Context context, String urlForPicture, String title) {
+		urlForPictureForThread = urlForPicture;
+
+
 
 		newsView = LayoutInflater.from(getContext());
 		inflatedViewNews = newsView.inflate(R.layout.view_for_inflating_news,
 				mainLayout, false);
-		
+		newsPicture = (ImageView) inflatedViewNews
+				.findViewById(R.id.newsImageView);
 
 		TextView textView1 = (TextView) inflatedViewNews
 				.findViewById(R.id.newsTextView);
@@ -43,21 +49,32 @@ public class InflatedViewForNewsPage extends LinearLayout  {
 		textView1.setLayoutParams(paramsExample);
 		textView1.setTextColor(Color.rgb(255, 168, 0));
 		textView1.setText(title);
-
+		new ImageLoaderTask().execute(urlForPictureForThread, newsPicture);
 		return inflatedViewNews;
 
 	}
 
-	public static ImageView setPicture() {
+	protected class ImageLoaderTask extends AsyncTask<Object, Void, Bitmap> {
+		String urlEndAwayString;
+		ImageView hostTeam;
+		Bitmap resizedBitmapFlag;
 
-		newsPicture = (ImageView) inflatedViewNews
-				.findViewById(R.id.newsImageView);
+		@Override
+		protected Bitmap doInBackground(Object... params) {
+			urlEndAwayString = (String) params[0];
+			hostTeam = (ImageView) params[1];
 
-	return newsPicture;
-	
+			Bitmap b = new DirectLoader().download(urlEndAwayString);
+		
+			resizedBitmapFlag = Bitmap.createScaledBitmap(b, MainActivity.width * 42 / 100, 250, true);
+			return resizedBitmapFlag;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			hostTeam.setImageBitmap(resizedBitmapFlag);
+		}
+
 	}
-	
-	
-	
 
 }
