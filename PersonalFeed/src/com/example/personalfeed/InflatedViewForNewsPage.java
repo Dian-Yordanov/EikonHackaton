ea043853b08c78de1a96249049f3051b37986840
Xynnect <dido.jordanov7@gmail.com>
2014-03-23 03:39:10 +0000

    next step is resizing

--- PersonalFeed/src/com/example/personalfeed/InflatedViewForNewsPage.java
+++ PersonalFeed/src/com/example/personalfeed/InflatedViewForNewsPage.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import com.novoda.imageloader.core.util.DirectLoader;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,7 +47,13 @@ public class InflatedViewForNewsPage extends LinearLayout {
 		LayoutParams paramsExample = new LayoutParams(
 				MainActivity.width * 42 / 100, 80, 1.0f);
 		paramsExample.setMargins(10, 0, 0, 0);
-
+		
+		  AssetManager mngr = getContext().getAssets();
+          Typeface face = Typeface. createFromAsset(mngr,
+                       "fonts/helveticaneue-webfont.ttf" );
+          textView1.setTypeface(face);
+          
+          
 		textView1.setLayoutParams(paramsExample);
 		textView1.setTextColor(Color.rgb(255, 168, 0));
 		textView1.setText(title);
